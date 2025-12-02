d1d618f32698fdbe389fbbe69d7531860ebe4270
autumn <cyancirrus@gmail.com>
2025-12-02 14:56:44 -0600

    parsing for day 2

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,36 @@
-use advent_of_code::solutions::day_one::{parser, secret_decoder_alpha, secret_decoder_beta};
+use std::{fs, error::Error};
+
+pub fn parser(path:&str) -> Result<Vec<(u32, u32)>, Box<dyn Error>>  {
+    let contents = match fs::read_to_string(path) {
+        Ok(p) => p,
+        Err(e) => return Err(format!("Invalid Path {}", e).into()),
+    };
+    let mut codes:Vec<(u32, u32)> = Vec::new();
+    for line in contents.lines() {
+        if line.trim().is_empty() {
+            continue;
+        }
+        let identifiers:Option<(&str, &str)> = line.split_once("-");
+        match identifiers {
+            Some((left, right)) => {
+                match (left.parse(), right.parse()) {
+                    (Ok(l), Ok(r)) => codes.push((l,r)),
+                    _ => return Err(format!("Invalid Code into integer").into())
+
+                }
+            },
+            _ => return Err(format!("Invalid Split for line code").into()),
+
+        }
+    }
+    Ok(codes)
+}
 
 fn main() {
-    let test_nums = vec![-100];
-    assert_eq!(&1, &secret_decoder_beta(&test_nums));
-
-    let test_nums = vec![-20, -30];
-    assert_eq!(&1, &secret_decoder_beta(&test_nums));
-
-    let test_nums = vec![-30, 80];
-    assert_eq!(&1, &secret_decoder_beta(&test_nums));
-
-    let test_nums = vec![-30, 280];
-    assert_eq!(&3, &secret_decoder_beta(&test_nums));
-
-    let test_nums = vec![-50, 0, 0];
-    assert_eq!(&1, &secret_decoder_beta(&test_nums));
-
-    let num_parse = parser("./data/day_1.txt");
-    match num_parse {
-        Ok(nums) => {
-            println!("Alpha secret is {}", secret_decoder_alpha(&nums));
-            println!("Beta secret is {}", secret_decoder_beta(&nums));
+    let code_parse = parser("./data/day_2.txt");
+    match code_parse {
+        Ok(codes) => {
+            println!("codes \n {:?}", &codes);
         }
         Err(e) => {
             println!("unsuccessful parse {}", e);
