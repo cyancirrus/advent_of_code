b907da1ed50e759a360ce0e2de5fe5a142f81ba8
autumn <cyancirrus@gmail.com>
2025-12-19 12:08:38 -0600

    running on tower

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@
 use std::time::Instant;
 use std::{error::Error, fs};
 use std::collections::{VecDeque, HashSet};
-use advent_of_code::parsers::day_ten::parser_bits;
+use advent_of_code::parsers::day_ten::{parser_bits, parser_ints};
 
 
 fn alpha_initialize_state(objective:u16, switches:&[u16]) -> usize {
-    let mut seen = HashSet::new();
+    let mut seen = HashSet::from([objective]);
     let mut queue = VecDeque::from([(objective, 0)]);
     while let Some((state, steps)) = queue.pop_front() {
         for s in switches {
@@ -28,9 +28,45 @@ fn alpha_initialize_all_state(reqs:&[(u16, Vec<u16>, Vec<u16>)]) -> usize {
     total
 }
 
-fn beta_intialize_state(switches:&[u16], joltage:Vec<u16>) {
+fn beta_initialize_state(switches:&[Vec<usize>], joltage:Vec<usize>) -> usize {
     let mut seen = HashSet::new();
-    seen.insert(vec![1,2,3]);
+    let mut non_zeros = 0;
+    for &j in &joltage {
+        if j != 0 { non_zeros +=1; }
+    }
+    let mut queue = VecDeque::from([(joltage, non_zeros, 0)]);
+    
+    while let Some((joltage, non_zeros, steps)) = queue.pop_back() {
+        for sw in switches {
+            let mut update = joltage.clone();
+            let mut update_non_zeros = non_zeros;
+            for &jdx in sw {
+                if update[jdx] == 0 { break; } else if update[jdx] == 1 {
+                    update_non_zeros -= 1;
+                }
+                update[jdx] -= 1;
+            }
+            if update_non_zeros == 0 {
+                return steps +1;
+            }
+            else if seen.insert(update.clone()) {
+                queue.push_front((update, update_non_zeros, steps+1));
+            }
+            
+        }
+    }
+    usize::MAX
+}
+
+fn beta_initialize_all_state(reqs:&[(Vec<Vec<usize>>, Vec<usize>)]) -> usize {
+    let mut total = 0;
+    let mut i = 0;
+    for (switches, joltage) in reqs {
+        println!("i {i:?}, joltage {joltage:?}");
+        total += beta_initialize_state(switches, joltage.to_vec());
+        i+=1;
+    }
+    total
 }
 
 fn main() {
@@ -47,6 +83,20 @@ fn main() {
             // let result = beta_find_max_rectangle(&mut p);
             // let time = start.elapsed();
             // println!("Beta version: {} in {:?}", result, time);
+        }
+        _ => {
+            println!("Error in parsing");
+        }
+    }
+    println!("-------------------------------------------------------------");
+    let reqs = parser_ints("./data/day_10.txt");
+    match reqs {
+        Ok(p) => {
+            let start = Instant::now();
+            let result = beta_initialize_all_state(&p);
+            let time = start.elapsed();
+            println!("Beta found at {result:?}");
+            println!("Beta version: {} in {:?}", result, time);
         }
         _ => {
             println!("Error in parsing");
